6c1cc2bb4e8dcb55482a927b5b66e213beb5bfce
buja <m48483@naver.com>
2024-11-19 17:03:04 +0900

    피보나치 0과 1의 호출 횟수 성공

--- BJ1003.java
+++ BJ1003.java
@@ -3,31 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BJ1003 {
-    static int zero;
-    static int one;
+    static Integer[][] result = new Integer[41][2]; //N은 40보다 작거나 같은 자연수 또는 0
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
 
+        result[0][0] = 1;
+        result[0][1] = 0;
+        result[1][0] = 0;
+        result[1][1] = 1;
+
         for (int i = 0; i < t; i++) {
-            zero = 0;
-            one = 0;
             int n = Integer.parseInt(br.readLine());
             fibonacci(n);
-            System.out.println(zero+" "+one);
+            sb.append(result[n][0]+" "+result[n][1]+"\n");
         }
+        System.out.println(sb);
     }
 
-    private static int fibonacci(int n){
-        if(n==0) {
-            zero++;
-            return 0;
+    private static Integer[] fibonacci(int n){
+        if(result[n][0] == null || result[n][1] == null) {
+            result[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
+            result[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
         }
-        else if(n==1) {
-            one++;
-            return 1;
-        }
-        else return fibonacci(n-1) + fibonacci(n-2);
+        return result[n];
     }
 }
