e1e94df749c2a992d4c769d13919bab8136b2516
buja <m48483@naver.com>
2024-11-20 17:11:11 +0900

    백준1463 정답

--- BJ1463.java
+++ BJ1463.java
@@ -13,10 +13,10 @@ public class BJ1463 {
 
         for (int i = 2; i <= n; i++) {
             result[i] = result[i-1]+1;
-            if (i%2==0){
+            if (i % 2 == 0){
                 result[i] = Math.min(result[i], result[i/2]+1);
             }
-            if (i%3==0){
+            if (i % 3 == 0){
                 result[i] = Math.min(result[i], result[i/3]+1);
             }
         }
